3065fe95bfef3072f3b5a4ff53f0a2be0196539e
jeandev09 <jean132309@gmail.com>
2024-10-29 12:21:55 -0500

    first commit

--- src/main/java/eu/kanade/i18n/I18nManager.java
+++ src/main/java/eu/kanade/i18n/I18nManager.java
@@ -1,18 +1,19 @@
 package eu.kanade.i18n;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  *
